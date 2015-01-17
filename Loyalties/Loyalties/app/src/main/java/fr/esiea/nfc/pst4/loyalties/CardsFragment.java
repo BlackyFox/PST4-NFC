4d5de5a65ff644c323841a3eab79c830f5dad757
BlackyFox <puissant@et.esiea.fr>
2015-01-17 21:22:22 +0100

    Démarrage code application pour le projet

---
+++ Loyalties/Loyalties/app/src/main/java/fr/esiea/nfc/pst4/loyalties/CardsFragment.java
@@ -0,0 +1,25 @@
+package fr.esiea.nfc.pst4.loyalties;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Antoine on 17/01/2015.
+ */
+public class CardsFragment extends Fragment {
+
+    View rootview;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootview = inflater.inflate(R.layout.fragment_cards, container, false);
+        return rootview;
+    }
+}
