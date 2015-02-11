998bbccb129aaac5fe73187b0cce63aa64c3898d
BlackyFox <puissant@et.esiea.fr>
2015-02-11 11:23:53 +0100

    Dans settings, ajout de la deletion du fichier de login

--- Loyalties/Loyalties/app/src/main/java/fr/esiea/nfc/pst4/loyalties/SettingsActivity.java
+++ Loyalties/Loyalties/app/src/main/java/fr/esiea/nfc/pst4/loyalties/SettingsActivity.java
@@ -1,12 +1,15 @@
 package fr.esiea.nfc.pst4.loyalties;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class SettingsActivity extends PreferenceActivity {
     private static final boolean ALWAYS_SIMPLE_PREFS = false;
@@ -16,6 +19,31 @@ public class SettingsActivity extends PreferenceActivity {
         super.onPostCreate(savedInstanceState);
 
         setupSimplePreferencesScreen();
+        Preference logOut = findPreference("LogOut");
+
+        logOut.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                deleteCreds();
+                deleteUser();
+                finish();
+                return true;
+            }
+        });
+    }
+
+    private void deleteUser() {
+        //TODO
+    }
+
+    private void deleteCreds() {
+        File f = new File(getApplicationContext().getFilesDir().getAbsolutePath() + "/userCredentials.berzerk");
+        if(f.length() > 0)
+            f.delete();
+        Toast.makeText(getApplicationContext(), "Data deleted", Toast.LENGTH_LONG).show();
+        Intent i = new Intent(this, LogInActivity.class);
+        startActivity(i);
     }
 
     private void setupSimplePreferencesScreen() {
