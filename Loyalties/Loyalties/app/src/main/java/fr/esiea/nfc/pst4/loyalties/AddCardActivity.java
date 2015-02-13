04304b39593ec589c445e54ce06c91f2a1f8cb81
BlackyFox <puissant@et.esiea.fr>
2015-02-13 15:19:34 +0100

    Ajout du DatePickerDialog dans AddCardActivity

--- Loyalties/Loyalties/app/src/main/java/fr/esiea/nfc/pst4/loyalties/AddCardActivity.java
+++ Loyalties/Loyalties/app/src/main/java/fr/esiea/nfc/pst4/loyalties/AddCardActivity.java
@@ -1,20 +1,59 @@
 package fr.esiea.nfc.pst4.loyalties;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
-public class AddCardActivity extends ActionBarActivity {
+public class AddCardActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    private Button butDate;
+    private TextView tvDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+
+        butDate = (Button)findViewById(R.id.buttonDate);
+        tvDate = (TextView)findViewById(R.id.textViewDate);
+
+        butDate.setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View v){
+        if(v == butDate){
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog dpd = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+                            // Display Selected date in textbox
+                            tvDate.setText(dayOfMonth + "/"
+                                    + (monthOfYear + 1) + "/" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            dpd.show();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
