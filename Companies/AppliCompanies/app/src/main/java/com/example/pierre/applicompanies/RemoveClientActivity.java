d2d54cf035a0c4b9b56cc84032eaf11a8b57fbc8
pecarlat <pierre.ecarlat@gmail.com>
2015-03-14 02:03:20 +0100

    ajout de remove client

--- Companies/AppliCompanies/app/src/main/java/com/example/pierre/applicompanies/RemoveClientActivity.java
+++ Companies/AppliCompanies/app/src/main/java/com/example/pierre/applicompanies/RemoveClientActivity.java
@@ -1,22 +1,49 @@
 package com.example.pierre.applicompanies;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.example.pierre.applicompanies.library_http.AsyncHttpClient;
+import com.example.pierre.applicompanies.library_http.AsyncHttpResponseHandler;
+import com.example.pierre.applicompanies.library_http.RequestParams;
 import com.example.pierre.applicompanies.objectsPackage.Company;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+import java.util.TimeZone;
 
 /**
  * Created by Pierre on 12/03/2015.
  */
-public class RemoveClientActivity extends Activity {
+public class RemoveClientActivity extends ListActivity {
 
+    ClientCustomAdapter adapter;
+    private List<ClientRowItem> clientRowItems;
     Company company;
+    String[][] clients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_remove_client);
+        setContentView(R.layout.activity_see_clients);
 
         int id = Integer.parseInt(getIntent().getStringExtra("id"));
         String name = getIntent().getStringExtra("name");
@@ -25,8 +52,210 @@ public class RemoveClientActivity extends Activity {
         String up_date = getIntent().getStringExtra("up_date");
         company = new Company(id, name, logo, card);
         company.setUp_date(up_date);
+
+        //getAllClients();
     }
 
+    @Override
+    protected void onResume() {
+        super.onRestart();
+        getAllClients();
+    }
+
+    // Traduit une chaîne de caractère au format JSON en format utf8_unicode
+    public static String decodeJSONString(String s) {
+        String decodeS = null;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '\\' && s.charAt(i + 1) == 'u') {
+                String key = s.substring(i, i + 6);
+                Properties p = new Properties();
+                try {
+                    p.load(new StringReader("key=" + key));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (decodeS != null) {
+                    decodeS = decodeS.concat(p.getProperty("key"));
+                } else {
+                    decodeS = p.getProperty("key");
+                }
+                i += 5;
+            } else {
+                if (decodeS != null) {
+                    decodeS = decodeS.concat(Character.toString(s.charAt(i)));
+                } else {
+                    decodeS = Character.toString(s.charAt(i));
+                }
+            }
+        }
+
+        return decodeS;
+    }
+
+    // Traduit la réponse du .php en ligne en tableau de string compréhensible
+    public HashMap<String, String> translateResponse(String response) {
+        String[] firstSep = response.split("\",\"");
+        HashMap<String, String> map = new HashMap<>();
+        String[] tmp;
+
+        for (int i = 0; i < firstSep.length; i++) {
+            tmp = firstSep[i].split("\":\"");
+            if (i == 0) tmp[0] = tmp[0].substring(3);
+            if (i == firstSep.length - 1) tmp[1] = tmp[1].substring(0, tmp[1].length() - 3);
+            map.put(tmp[0], decodeJSONString(tmp[1]));
+        }
+
+        return map;
+    }
+
+    public void getAllClients() {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+
+        ArrayList<HashMap<String, String>> wordList = new ArrayList<>();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("company_id", Integer.toString(company.getId()));
+        wordList.add(map);
+
+        Gson gson = new GsonBuilder().create();
+        params.put("getClientsJSON", gson.toJson(wordList));
+
+        // On envoie username / password pour vérifier si lesidentifiants sont bons selon la bdd en ligne
+        client.post("http://www.pierre-ecarlat.com/newSql/getclients.php", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String response = null;
+                clients = null;
+                System.out.println("onsuccess");
+
+                try {
+                    response = new String(responseBody, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    JSONArray arr = new JSONArray(response);
+                    HashMap<String, String> map = translateResponse(response);
+                    System.out.println("response : " + response);
+
+                    if (map.get("works").equals("yes") && Integer.parseInt(map.get("nb_of_clients")) != 0) {
+                        clients = new String[6][Integer.parseInt(map.get("nb_of_clients"))];
+                        for (int i = 0; i < Integer.parseInt(map.get("nb_of_clients")); i++) {
+                            clients[0][i] = map.get("client" + i + "_name");
+                            clients[1][i] = map.get("client" + i + "_first_name");
+                            clients[2][i] = map.get("client" + i + "_username");
+                            clients[3][i] = map.get("client" + i + "_num_client");
+                            clients[4][i] = map.get("client" + i + "_nb_points");
+                            clients[5][i] = map.get("client" + i + "_last_used");
+                        }
+                    } else {
+                        clients[0][0] = "No clients";
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                if (statusCode == 404) {
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                } else if (statusCode == 500) {
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet]", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                if (clients != null) {
+                    addItemsOnList();
+                } else {
+                    Toast.makeText(getApplicationContext(), "No clients.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void addItemsOnList() {
+        clientRowItems = new ArrayList<>();
+
+        for (int i = 0; i < clients[0].length; i++) {
+            ClientRowItem items = new ClientRowItem(clients[0][i], clients[1][i], clients[2][i], clients[3][i], Integer.parseInt(clients[4][i]), Integer.parseInt(clients[5][i]));
+            clientRowItems.add(items);
+        }
+
+        adapter = new ClientCustomAdapter(this, clientRowItems);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView parent, View view, int position, long id) {
+        if (!clients[0][position].equals("No clients")) {
+            removeClient(clients[3][position]);
+            this.finish();
+        }
+    }
+
+    public void removeClient(String num_client) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        System.out.println("ici -> " + num_client);
+
+        ArrayList<HashMap<String, String>> wordList = new ArrayList<>();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("num_client", num_client);
+        map.put("up_date", Calendar.getInstance(TimeZone.getDefault()).getTime().toString());
+        wordList.add(map);
+
+        Gson gson = new GsonBuilder().create();
+        params.put("removeClientJSON", gson.toJson(wordList));
+
+        // On envoie username / password pour vérifier si lesidentifiants sont bons selon la bdd en ligne
+        client.post("http://www.pierre-ecarlat.com/newSql/removeclient.php", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String response = null;
+                clients = null;
+                System.out.println("onsuccess");
+
+                try {
+                    response = new String(responseBody, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    JSONArray arr = new JSONArray(response);
+                    HashMap<String, String> map = translateResponse(response);
+                    System.out.println("response : " + response);
+
+                    if (map.get("works").equals("yes")) {
+                        Toast.makeText(getApplicationContext(), "Works !", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Doesn't works", Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                if (statusCode == 404) {
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                } else if (statusCode == 500) {
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet]", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
